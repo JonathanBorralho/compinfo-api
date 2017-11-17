9c7aab825fe0c0c866aef914b86968dc1511e0b4
Jonathan Borralho <jonathan.borralho@gmail.com>
2017-11-17 01:35:56 -0200

    Cross origins

--- src/main/java/com/pitagoras/compinfo/resource/CursoResource.java
+++ src/main/java/com/pitagoras/compinfo/resource/CursoResource.java
@@ -1,6 +1,7 @@
 package com.pitagoras.compinfo.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pitagoras.compinfo.model.Curso;
 import com.pitagoras.compinfo.repository.CursoRepository;
 
+@CrossOrigin(origins = "**")
 @RestController
 public class CursoResource {
 	
